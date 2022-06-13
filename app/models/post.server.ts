d4c5d29866e6f7858665821bed7de2976f94c9af
Rowley Thompson <rjw@outlookenergy.co.uk>
2022-06-13 20:02:29 +0100

    form validation

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,4 +1,6 @@
+import { createResponseComposition } from "msw";
 import { prisma } from "~/db.server"
+import type { Post } from "@prisma/client";
 
 //The ~/db.server import is importing the file at app/db.server.ts. 
 // The ~ is a fancy alias to the app directory so you don't have to worry about how many 
@@ -12,4 +14,8 @@ export async function getPosts() {
 
 export async function getPost(slug: string) {
     return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">){
+    return prisma.post.create({ data: post })
 }
